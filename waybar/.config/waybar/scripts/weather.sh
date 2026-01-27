f304987a0e5bb7b83b75c8811a5e6cc62163be18
SziliAti <mail@sziliati.com>
2026-01-27 11:22:39 +0100

    add simple wather widget

--- waybar/.config/waybar/scripts/weather.sh
+++ waybar/.config/waybar/scripts/weather.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Fetch weather from wttr.in and output JSON for Waybar
-curl -s "wttr.in/?format=j1" | jq -r '
+curl -s "wttr.in/Sulysap?format=j1" | jq -r '
   {
     text: ("🌦️ " + (.current_condition[0].temp_C) + "°C"),
     tooltip: ("Feels: " + .current_condition[0].FeelsLikeC + "°C")
